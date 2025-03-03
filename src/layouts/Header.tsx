f5e4b37212909ab4e1650c05849adc4b62ebf504
judahwon <hwondaa@gmail.com>
2025-03-03 23:12:40 +0900

    feat: meta tag 추가

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,7 +1,7 @@
 
 function Header() {
   return (
-    <div className={'z-50 col-span-3 grid size-full grid-cols-[55px,_180px,_1fr] bg-main'}>
+    <div className={'z-50 col-span-3 grid size-full bg-main'}>
       <div className={'flex items-center justify-center text-[20px] font-bold text-light'}>{'hwon-da.githu.io'}</div>
     </div>
   );
