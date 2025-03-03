aee081f3199d579b9737cc9ca52dc81e740060b8
judahwon <hwondaa@gmail.com>
2025-03-03 23:17:23 +0900

    Remove dist folder from git tracking

--- dist/assets/SecondView-DSMQHPWS.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Bz09jjKG.js";const s=()=>e.jsx("div",{className:"flex size-full",children:e.jsx("div",{className:"z-50 flex h-full w-[350px] flex-col bg-white p-4",children:"2번째 페이지"})});export{s as default};
